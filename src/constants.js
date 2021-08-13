549b166bd7d53b36ecc4c6215f3ba38875500078
Tomi Jokkeenhaara <tomi.jokkeenhaara@tuni.fi>
2021-08-13 13:26:38 +0300

    Changed endpoints from localhost to 127.0.0.1

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const analysisEndpoint = "http://localhost:5000";
-export const chatEndpoint = "http://localhost:3010";
+export const analysisEndpoint = "http://127.0.0.1:5000";
+export const chatEndpoint = "http://127.0.0.1:3010";
 export const annotations = !!process.env.REACT_APP_ANNOTATIONS;
 export const messageFeedbackStrings = {
     negative: "Tästä viestistä on tunnistettu negatiivisia tunteita.",
